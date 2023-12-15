70dd6b842898ea572d2aa2baa16e62ff278d454e
vinhnado0231 <vinh.nd0231@gmail.com>
2023-12-15 11:13:40 +0700

    done setup docker

--- vuejs_tutorial/weather-app/weather-app/src/App.vue
+++ vuejs_tutorial/weather-app/weather-app/src/App.vue
@@ -11,21 +11,6 @@
 <script setup>
 import HeaderVue from './components/Header/Header.vue';
 import FooterVue from './components/Footer/Footer.vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from './components/Home.vue';
-import Login from './components/Login.vue';
-import Weather from './components/Weather.vue';
-
-// Tạo router instance
-// export const router = createRouter({
-// 	history: createWebHistory(),
-// 	routes: [
-// 		{ path: '/', component: Home },
-// 		{ path: '/login', component: Login },
-// 		{ path: '/weather', component: Weather },
-// 	],
-// });
-//
 </script>
 
 <style scoped></style>
